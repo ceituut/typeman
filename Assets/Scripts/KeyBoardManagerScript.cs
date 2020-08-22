3fe9a1daf49cd84b4858f0760e35ae1b27b1b9db
alireza naderitomateri <alirezanaderitomateri@gmailcom>
2020-08-22 14:05:07 +0430

    code-tutorial section UI-KeyBoard done by Alireza Naderi

---
+++ Assets/Scripts/KeyBoardManagerScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyBoardManagerScript : singleton<KeyBoardManagerScript>
+{
+    public bool IsKeyBoardSectionEnabled;
+    public GameObject keybard;
+    
+   
+    public bool IsKeyBoardSystemReady()
+    {
+        if(IsKeyBoardSectionEnabled && (keybard==null ? false : true))
+          Debug.Log("Keyboard System Run.");
+          else
+          {
+              if(!IsKeyBoardSectionEnabled)
+              {
+                  Debug.Log("keyboard system is not enabled!");
+              }
+              else
+              if(keybard==null)
+              {
+                  Debug.Log("keyboard GameObject Variable is null!");
+              }
+          }
+        return (IsKeyBoardSectionEnabled && (keybard==null ? false : true));
+    }
+}
---
+++ Assets/Scripts/UiKeyboardScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UiKeyboardScript : MonoBehaviour
+{
+    public KeyCode mykey;//initiate in the unity editor
+   private void Update()
+   {
+       PressRelatedBtn();
+   }
+   private void PressRelatedBtn()
+   {
+       if(KeyBoardManagerScript.instance.IsKeyBoardSectionEnabled)
+       {
+          if(mykey==currentBtnKeyCode())
+          {
+              Debug.Log(mykey);///////
+              //run animation on correct key(code here)
+          }
+          
+       }
+   }
+   private KeyCode currentBtnKeyCode()
+   {
+       var e=System.Enum.GetNames(typeof(KeyCode)).Length;
+        for(int i = 0; i < e; i++)
+             {
+                 if(Input.GetKey((KeyCode)i))
+                 {
+                     return (KeyCode)i;
+                 }
+             }
+             return KeyCode.None;
+   }
+}
