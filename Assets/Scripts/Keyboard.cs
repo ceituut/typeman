1746d935c347ab0ac6bcb80c70a2bd7c21b72a03
Amir Goodarzi <amirgoodarzi75@gmail.com>
2020-09-15 00:11:22 +0430

    Using ScriptableObject for changing keyboard failed

---
+++ Assets/Scripts/Keyboard.cs
@@ -0,0 +1,422 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class KeyboardDef
+{
+    public enum languages { English , Farsi }
+    public enum keyboardTypes 
+    { 
+        WindowsDesktop , WindowsErgonomic1 , WindowsErgonomic2 , 
+        WindowsPortable1 , WindowsPortable2 , WindowsPortable3 , WindowsPocket1 ,
+        MacDesktop , MacPortable
+    };
+    public enum numberOfKeysTypes { key104 , key105 , key107 };
+    public enum enterTypes { flat , high , big };
+}
+
+
+
+
+public abstract class Keyboard
+{
+    // Fields
+        // Keyboard (without/with Shift key) string values 
+    protected List<string> primaryKeyList;
+    protected List<string> secondaryKeyList;
+    // limited number of keys in each row
+    protected List<int> limitedRows;
+
+    // Properties
+    public List<string> GetPrimaryKeyList { get => primaryKeyList;}
+    public List<string> GetSecondaryKeyList { get => secondaryKeyList;}
+    public List<int> GetLimitedRows { get => limitedRows;}
+
+    // Methods
+    public abstract void InitializeDefualt();
+    public abstract void InitializeRowLimits();
+        // Number of keys 
+    public abstract void MakeIt104Key();
+    public abstract void MakeIt105Key();
+    public abstract void MakeIt107Key();
+        // Enter Types
+    public abstract void MakeEnterFlat();
+    public abstract void MakeEnterHigh();
+    public abstract void MakeEnterBig();
+}
+
+
+
+
+public class WinDesktop : Keyboard
+{
+    // Constructor
+    public WinDesktop()
+    {
+        InitializeDefualt();
+    }
+
+    // Methods
+    public override void InitializeDefualt()
+    {
+        primaryKeyList = new List<string>
+        {
+            "`","1","2","3","4","5","6","7","8","9","0","-","=","Backspace",
+            "Tab","q","w","e","r","t","y","u","i","o","p","[","]","\\",
+            "Caps Lock","a","s","d","f","g","h","j","k","l",";","'","Enter",
+            "Shift","z","x","c","v","b","n","m",",",".","/","Shift",
+            "Ctrl","Win","Alt","Space","AltGr","Win","Menu","Ctrl"
+        };
+        secondaryKeyList = new List<string>
+        {
+            "~","!","@","#","$","%","^","&","*","(",")","_","+","Backspace",
+            "Tab","Q","W","E","R","T","Y","U","I","O","P","{","}","|",
+            "Caps Lock","A","S","D","F","G","H","J","K","L",":","\"","Enter",
+            "Shift","Z","X","C","V","B","N","M","<",">","?","Shift",
+            "Ctrl","Win","Alt","Space","AltGr","Win","Menu","Ctrl"
+        };
+        InitializeRowLimits();
+    }
+    public override void InitializeRowLimits()
+    {
+        int row0Limit = primaryKeyList.IndexOf("Backspace") + 1;
+        int row1Limit = primaryKeyList.IndexOf("\\") + 1 - row0Limit;
+        int row2Limit = primaryKeyList.IndexOf("Enter") + 1 - row1Limit;
+        int row3Limit = primaryKeyList.IndexOf("Shift") + 1 - row2Limit;
+        int row4Limit = primaryKeyList.IndexOf("Ctrl") + 1 - row3Limit;
+        limitedRows = new List<int>{row0Limit,row1Limit,row2Limit,row3Limit,row4Limit};
+    }
+    public override void MakeIt104Key()
+    {
+        InitializeDefualt();
+    }
+    public override void MakeIt105Key()
+    {
+        int insertionIndex = primaryKeyList.IndexOf("Shift");
+        string newPrimaryKey = "\\";
+        string newSecondaryKey = "|";
+        primaryKeyList.Insert(insertionIndex,newPrimaryKey);
+        secondaryKeyList.Insert(insertionIndex,newSecondaryKey);
+        // Update row limits
+        limitedRows[4] += 1;
+    }
+    public override void MakeIt107Key()
+    {
+        MakeIt105Key();
+        int insertionIndex = primaryKeyList.IndexOf("/");
+        string newPrimaryKey = "   ";
+        string newSecondaryKey = "   ";
+        primaryKeyList.Insert(insertionIndex,newPrimaryKey);
+        secondaryKeyList.Insert(insertionIndex,newSecondaryKey);
+        // Update row limits
+        limitedRows[4] += 1;
+    }
+    public override void MakeEnterFlat()
+    {
+        InitializeDefualt();
+    }
+    public override void MakeEnterHigh()
+    {
+        int firstIndex = primaryKeyList.IndexOf("Enter");
+        int secondIndex = primaryKeyList.IndexOf("\\");
+        // Swap primary keys
+        string tempKey = primaryKeyList[firstIndex];
+        primaryKeyList[firstIndex] = primaryKeyList[secondIndex];
+        primaryKeyList[secondIndex] = tempKey;
+        // Swap secondary keys
+        tempKey = secondaryKeyList[firstIndex];
+        secondaryKeyList[firstIndex] = secondaryKeyList[secondIndex];
+        secondaryKeyList[secondIndex] = tempKey;
+    }
+    public override void MakeEnterBig()
+    {
+        MakeEnterHigh();
+        int insertionIndex = primaryKeyList.IndexOf("=");
+        // Remove primary & secondary string of target key
+        primaryKeyList.Remove("\\");
+        secondaryKeyList.Remove("|");
+        // Insert them after = key
+        primaryKeyList.Insert(insertionIndex,"\\");
+        secondaryKeyList.Insert(insertionIndex,"|");
+        // Update row limits
+        limitedRows[0] += 1;
+        limitedRows[2] -= 1;
+    }
+}
+
+
+
+
+public class WindPortable1 : WinDesktop
+{
+    public WindPortable1()
+    {
+        InitializeDefualt();
+    }
+    public override void InitializeDefualt()
+    {
+        primaryKeyList = new List<string>
+        {
+            "`","1","2","3","4","5","6","7","8","9","0","-","=","Backspace",
+            "Tab","q","w","e","r","t","y","u","i","o","p","[","]","\\",
+            "Caps Lock","a","s","d","f","g","h","j","k","l",";","'","Enter",
+            "Shift","z","x","c","v","b","n","m",",",".","/","Shift",
+            "Ctrl","Fn","Win","Alt","Space","AltGr","Prnt Scr","Ctrl"
+        };
+        secondaryKeyList = new List<string>
+        {
+            "~","!","@","#","$","%","^","&","*","(",")","_","+","Backspace",
+            "Tab","Q","W","E","R","T","Y","U","I","O","P","{","}","|",
+            "Caps Lock","A","S","D","F","G","H","J","K","L",":","\"","Enter",
+            "Shift","Z","X","C","V","B","N","M","<",">","?","Shift",
+            "Ctrl","Fn","Win","Alt","Space","AltGr","Prnt Scr","Ctrl"
+        };
+        InitializeRowLimits();
+    }
+}
+
+
+
+
+public class WindPortable2 : WinDesktop
+{
+    public WindPortable2()
+    {
+        InitializeDefualt();
+    }
+    public override void InitializeDefualt()
+    {
+        primaryKeyList = new List<string>
+        {
+            "`","1","2","3","4","5","6","7","8","9","0","-","=","Backspace",
+            "Tab","q","w","e","r","t","y","u","i","o","p","[","]","\\",
+            "Caps Lock","a","s","d","f","g","h","j","k","l",";","'","Enter",
+            "Shift","z","x","c","v","b","n","m",",",".","/","Shift",
+            "Ctrl","Fn","Win","Alt","Space","AltGr","Ctrl"
+        };
+        secondaryKeyList = new List<string>
+        {
+            "~","!","@","#","$","%","^","&","*","(",")","_","+","Backspace",
+            "Tab","Q","W","E","R","T","Y","U","I","O","P","{","}","|",
+            "Caps Lock","A","S","D","F","G","H","J","K","L",":","\"","Enter",
+            "Shift","Z","X","C","V","B","N","M","<",">","?","Shift",
+            "Ctrl","Fn","Win","Alt","Space","AltGr","Ctrl"
+        };
+        InitializeRowLimits();
+    }
+}
+
+
+
+
+public class WindPortable3 : WindPortable1
+{
+    public WindPortable3()
+    {
+        base.InitializeDefualt();
+    }
+}
+
+
+
+
+public class WindPocket1 : WindPortable1
+{
+    public WindPocket1()
+    {
+        base.InitializeDefualt();
+    }
+}
+
+
+
+
+public class WinErgonomic1 : WinDesktop
+{
+    public WinErgonomic1()
+    {
+        base.InitializeDefualt();
+    }
+}
+
+
+
+
+public class WinErgonomic2 : WinDesktop
+{
+    public WinErgonomic2()
+    {
+        InitializeDefualt();
+    }
+    public override void InitializeDefualt()
+    {
+        primaryKeyList = new List<string>
+        {
+            "Backspace","1","2","3","4","5","6","7","8","9","0","-","=",
+            "Tab","q","w","e","r","t","y","u","i","o","p","[","]",
+            "Caps Lock","`","a","s","d","f","g","h","j","k","l",";","'","\\",
+            "Shift","z","x","c","v","b","n","m",",",".","/","Shift",
+            "Ctrl","Alt","Backspace","Delete","Enter","Space","AltGr","Ctrl"
+        };
+        secondaryKeyList = new List<string>
+        {
+            "Backspace","!","@","#","$","%","^","&","*","(",")","_","+",
+            "Tab","Q","W","E","R","T","Y","U","I","O","P","{","}",
+            "Caps Lock","`","A","S","D","F","G","H","J","K","L",":","\"","|",
+            "Shift","Z","X","C","V","B","N","M","<",">","?","Shift",
+            "Ctrl","Alt","Backspace","Delete","Enter","Space","AltGr","Ctrl"
+        };
+        InitializeRowLimits();
+    }
+    public override void InitializeRowLimits()
+    {
+        int row0Limit = primaryKeyList.IndexOf("=") + 1;
+        int row1Limit = primaryKeyList.IndexOf("]") + 1 - row0Limit;
+        int row2Limit = primaryKeyList.IndexOf("\\") + 1 - row1Limit;
+        int row3Limit = primaryKeyList.IndexOf("Shift") + 1 - row2Limit;
+        int row4Limit = primaryKeyList.IndexOf("Ctrl") + 1 - row3Limit;
+        limitedRows = new List<int>{row0Limit,row1Limit,row2Limit,row3Limit,row4Limit};
+    }
+    public override void MakeEnterFlat()
+    {
+        // Do nothing
+    }
+    public override void MakeEnterHigh()
+    {
+        // Do nothing
+    }
+    public override void MakeEnterBig()
+    {
+        // Do nothing
+    }
+}
+
+
+
+
+public class MacDesktop : Keyboard
+{
+    // Constructor
+    public MacDesktop()
+    {
+        InitializeDefualt();
+    }
+    public override void InitializeDefualt()
+    {
+        primaryKeyList = new List<string>
+        {
+            "`","1","2","3","4","5","6","7","8","9","0","-","=","delete",
+            "tab","q","w","e","r","t","y","u","i","o","p","[","]","\\",
+            "caps lock","a","s","d","f","g","h","j","k","l",";","'","return",
+            "shift","z","x","c","v","b","n","m",",",".","/","shift",
+            "control","option","command","space","command","option","control"
+        };
+        secondaryKeyList = new List<string>
+        {
+            "~","!","@","#","$","%","^","&","*","(",")","_","+","delete",
+            "tab","Q","W","E","R","T","Y","U","I","O","P","{","}","|",
+            "caps lock","A","S","D","F","G","H","J","K","L",":","\"","return",
+            "shift","Z","X","C","V","B","N","M","<",">","?","shift",
+            "control","option","command","space","command","option","control"
+        };
+        InitializeRowLimits();
+    }
+    public override void InitializeRowLimits()
+    {
+        int row0Limit = primaryKeyList.IndexOf("delete") + 1;
+        int row1Limit = primaryKeyList.IndexOf("\\") + 1 - row0Limit;
+        int row2Limit = primaryKeyList.IndexOf("return") + 1 - row1Limit;
+        int row3Limit = primaryKeyList.IndexOf("shift") + 1 - row2Limit;
+        int row4Limit = primaryKeyList.IndexOf("control") + 1 - row3Limit;
+        limitedRows = new List<int>{row0Limit,row1Limit,row2Limit,row3Limit,row4Limit};
+    }
+    public override void MakeIt104Key()
+    {
+        InitializeDefualt();
+    }
+    public override void MakeIt105Key()
+    {
+        int insertionIndex = primaryKeyList.IndexOf("shift");
+        string newPrimaryKey = "\\";
+        string newSecondaryKey = "|";
+        primaryKeyList.Insert(insertionIndex,newPrimaryKey);
+        secondaryKeyList.Insert(insertionIndex,newSecondaryKey);
+    }
+    public override void MakeIt107Key()
+    {
+        MakeIt105Key();
+        int insertionIndex = primaryKeyList.IndexOf("/");
+        string newPrimaryKey = "   ";
+        string newSecondaryKey = "   ";
+        primaryKeyList.Insert(insertionIndex,newPrimaryKey);
+        secondaryKeyList.Insert(insertionIndex,newSecondaryKey);
+    }
+    public override void MakeEnterFlat()
+    {
+        InitializeDefualt();
+    }
+    public override void MakeEnterHigh()
+    {
+        int firstIndex = primaryKeyList.IndexOf("return");
+        int secondIndex = primaryKeyList.IndexOf("\\");
+        // Swap primary keys
+        string tempKey = primaryKeyList[firstIndex];
+        primaryKeyList[firstIndex] = primaryKeyList[secondIndex];
+        primaryKeyList[secondIndex] = tempKey;
+        // Swap secondary keys
+        tempKey = secondaryKeyList[firstIndex];
+        secondaryKeyList[firstIndex] = secondaryKeyList[secondIndex];
+        secondaryKeyList[secondIndex] = tempKey;
+    }
+    public override void MakeEnterBig()
+    {
+        MakeEnterHigh();
+        int insertionIndex = primaryKeyList.IndexOf("=");
+        // Remove primary & secondary string of target key
+        primaryKeyList.Remove("\\");
+        secondaryKeyList.Remove("|");
+        // Insert them after = key
+        primaryKeyList.Insert(insertionIndex,"\\");
+        secondaryKeyList.Insert(insertionIndex,"|");
+    }
+}
+
+
+
+
+public class MacPortable : MacDesktop
+{
+    public MacPortable()
+    {
+        InitializeDefualt();
+    }
+    public override void InitializeDefualt()
+    {
+        primaryKeyList = new List<string>
+        {
+            "`","1","2","3","4","5","6","7","8","9","0","-","=","delete",
+            "tab","q","w","e","r","t","y","u","i","o","p","[","]","\\",
+            "caps lock","a","s","d","f","g","h","j","k","l",";","'","return",
+            "shift","z","x","c","v","b","n","m",",",".","/","shift",
+            "fn","control","option","command","space","command","option"
+        };
+        secondaryKeyList = new List<string>
+        {
+            "~","!","@","#","$","%","^","&","*","(",")","_","+","delete",
+            "tab","Q","W","E","R","T","Y","U","I","O","P","{","}","|",
+            "caps lock","A","S","D","F","G","H","J","K","L",":","\"","return",
+            "shift","Z","X","C","V","B","N","M","<",">","?","shift",
+            "fn","control","option","command","space","command","option"
+        };
+        InitializeRowLimits();
+    }
+    public override void InitializeRowLimits()
+    {
+        int row0Limit = primaryKeyList.IndexOf("delete") + 1;
+        int row1Limit = primaryKeyList.IndexOf("\\") + 1 - row0Limit;
+        int row2Limit = primaryKeyList.IndexOf("return") + 1 - row1Limit;
+        int row3Limit = primaryKeyList.IndexOf("shift") + 1 - row2Limit;
+        int row4Limit = primaryKeyList.IndexOf("option") + 1 - row3Limit;
+        limitedRows = new List<int>{row0Limit,row1Limit,row2Limit,row3Limit,row4Limit};
+    }
+}
--- Assets/Scripts/KeyboardEdition.cs
+++ Assets/Scripts/KeyboardEdition.cs
@@ -2,28 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName="Keyboard-En-Win-Desktop-104-enter-flat",menuName="TypeMan/Keyboard")]
+[CreateAssetMenu(fileName="Keyboard-En-WinDesktop-104key-enterflat",menuName="TypeMan/Keyboard")]
 public class KeyboardEdition : ScriptableObject
 {
     // Fields
+        // Keyboard object
+    private Keyboard keyboard;
         // Keyboard (without/with Shift key) string values 
     [SerializeField] private List<string> primaryKeyList;
     [SerializeField] private List<string> secondaryKeyList;
-        // Type Definitions 
-    private enum languages { English , Farsi };
-    private enum osTypes { Wind , Mac };
-    private enum keyboardTypes 
-    { 
-        Desktop , Ergonomic1 , Ergonomic2 , Portable1 , Portable2 , Portable3 , Pocket1
-    };
-    private enum numberOfKeysTypes { key104 , key105 , key107 };
-    private enum enterTypes { flat , high , big };
         // Type fields
-    [SerializeField] private languages languageType;
-    [SerializeField] private osTypes currentOSType;
-    [SerializeField] private keyboardTypes currentKeyboardTye;
-    [SerializeField] private numberOfKeysTypes currentNumberOfKeys;
-    [SerializeField] private enterTypes currentEnterType;
+    [SerializeField] private KeyboardDef.languages languageType;
+    [SerializeField] private KeyboardDef.keyboardTypes currentKeyboardType;
+    [SerializeField] private KeyboardDef.numberOfKeysTypes currentNumberOfKeys;
+    [SerializeField] private KeyboardDef.enterTypes currentEnterType;
         // limited number of keys in each row
     private List<int> rowLimits;
 
@@ -33,45 +25,199 @@ public class KeyboardEdition : ScriptableObject
     public List<string> SecondaryKeyList { get => secondaryKeyList; set => secondaryKeyList = value; }
     public List<int> GetRowLimits { get => rowLimits;}
         // Types
-    private osTypes CurrentOSType { get => currentOSType; set => currentOSType = value; }
-    private keyboardTypes CurrentKeyboardTye { get => currentKeyboardTye; set => currentKeyboardTye = value; }
-    private numberOfKeysTypes CurrentNumberOfKeys { get => currentNumberOfKeys; set => currentNumberOfKeys = value; }
-    private enterTypes CurrentEnterType { get => currentEnterType; set => currentEnterType = value; }
-
+    private KeyboardDef.languages LanguageType { get => languageType; set => languageType = value; }
+    private KeyboardDef.keyboardTypes CurrentKeyboardType 
+    { 
+        get { return currentKeyboardType;} 
+        set
+        {
+            UpdateKeyboardType(value);
+            currentKeyboardType = value;
+        }
+    }
+    private KeyboardDef.numberOfKeysTypes CurrentNumberOfKeys 
+    {
+        get { return currentNumberOfKeys;} 
+        set
+        {
+            UpdateKeys(value,currentEnterType);
+            rowLimits = keyboard.GetLimitedRows;
+            currentNumberOfKeys = value;
+        }
+    }
+    private KeyboardDef.enterTypes CurrentEnterType
+    {
+        get { return currentEnterType; }
+        set 
+        {
+            UpdateKeys(currentNumberOfKeys,value);
+            rowLimits = keyboard.GetLimitedRows;
+            currentEnterType = value;
+        }
+    }
     // Methods
     private void Awake() 
     {
-        rowLimits = new List<int>();
-        primaryKeyList = new List<string>
-        {
-            "`","1","2","3","4","5","6","7","8","9","0","-","=","Backspace",
-            "Tab","q","w","e","r","t","y","u","i","o","p","[","]","\\",
-            "Caps Lock","a","s","d","f","g","h","j","k","l",";","'","Enter",
-            "Shift","z","x","c","v","b","n","m",",",".","/","Shift",
-            "Ctrl","Win","Alt","Space","AltGr","Win","Menu","Ctrl"
-        };
-        secondaryKeyList = new List<string>
-        {
-            "~","!","@","#","$","%","^","&","*","(",")","_","+","Backspace",
-            "Tab","Q","W","E","R","T","Y","U","I","O","P","{","}","|",
-            "Caps Lock","A","S","D","F","G","H","J","K","L",":","\"","Enter",
-            "Shift","Z","X","C","V","B","N","M","<",">","?","Shift",
-            "Ctrl","Win","Alt","Space","AltGr","Win","Menu","Ctrl"
-        };
-        rowLimits.Add(14);
-        rowLimits.Add(14);
-        rowLimits.Add(13);
-        rowLimits.Add(12);
-        rowLimits.Add(8);
+        InitializeDefaultEdition();
     }
+    private void InitializeDefaultEdition()
+    {
+        InitializeTypes();
+        InitializeKeyLists();
+        rowLimits = keyboard.GetLimitedRows;
+    }
+    private void InitializeKeyLists()
+    {
+        keyboard = new WinDesktop();
+        keyboard.MakeIt104Key();
+        keyboard.MakeEnterFlat();
+        primaryKeyList = keyboard.GetPrimaryKeyList;
+        secondaryKeyList = keyboard.GetSecondaryKeyList;
+    }
+    private void InitializeTypes()
+    {
+        languageType = KeyboardDef.languages.English;
+        CurrentKeyboardType = KeyboardDef.keyboardTypes.WindowsDesktop;
+        CurrentNumberOfKeys = KeyboardDef.numberOfKeysTypes.key104;
+        CurrentEnterType = KeyboardDef.enterTypes.flat;
+    }
+    private void UpdateNumberOfKeys(KeyboardDef.numberOfKeysTypes thisNumberOfKeysType)
+    {
+        switch (thisNumberOfKeysType)
+        {
+            case KeyboardDef.numberOfKeysTypes.key105 :
+                keyboard.MakeIt105Key();
+                break;
+            case KeyboardDef.numberOfKeysTypes.key107 :
+                keyboard.MakeIt107Key();
+                break;
+            default:
+                break;
+        }
+    }
+    private void UpdateEnterTypes(KeyboardDef.enterTypes thisEnterType)
+    {
+        switch (thisEnterType)
+        {
+            case KeyboardDef.enterTypes.high :
+                keyboard.MakeEnterHigh();
+                break;
+            case KeyboardDef.enterTypes.big :
+                keyboard.MakeEnterBig();
+                break;
+            default:
+                break;
+        }
 
-    public void MakeIt105Key()
-    {
-        // After first Shift , Add \
     }
-    public void MakeIt107Key()
+    private void UpdateKeys(KeyboardDef.numberOfKeysTypes thisNumberOfKeysType , KeyboardDef.enterTypes thisEnterType)
     {
-        // After first Shift , Add \
-        // After / , Add a empty key
+        bool is104Keys = ( thisNumberOfKeysType == KeyboardDef.numberOfKeysTypes.key104 );
+        bool isEnterFlat = ( thisEnterType == KeyboardDef.enterTypes.flat );
+        bool isNeedTwoInitialization = (is104Keys && isEnterFlat);
+        if ( isNeedTwoInitialization )
+        {
+            keyboard.InitializeDefualt();
+        }
+        else
+        {
+            keyboard.InitializeDefualt();
+            UpdateNumberOfKeys(thisNumberOfKeysType);
+            UpdateEnterTypes(thisEnterType);
+        }
+    }
+    private void UpdateKeyboardType(KeyboardDef.keyboardTypes thisKeyboardType)
+    {
+        switch (thisKeyboardType)
+        {
+            case KeyboardDef.keyboardTypes.WindowsDesktop :
+                {
+                    keyboard = new WinDesktop();
+                    keyboard.MakeIt104Key();
+                    keyboard.MakeEnterFlat();
+                    primaryKeyList = keyboard.GetPrimaryKeyList;
+                    secondaryKeyList = keyboard.GetSecondaryKeyList; 
+                    break;
+                }
+            case KeyboardDef.keyboardTypes.WindowsPortable1 :
+                {
+                    keyboard = new WindPortable1();
+                    keyboard.MakeIt104Key();
+                    keyboard.MakeEnterFlat();
+                    primaryKeyList = keyboard.GetPrimaryKeyList;
+                    secondaryKeyList = keyboard.GetSecondaryKeyList; 
+                    break;
+                }
+            case KeyboardDef.keyboardTypes.WindowsPortable2 :
+                {
+                    keyboard = new WindPortable2();
+                    keyboard.MakeIt104Key();
+                    keyboard.MakeEnterFlat();
+                    primaryKeyList = keyboard.GetPrimaryKeyList;
+                    secondaryKeyList = keyboard.GetSecondaryKeyList; 
+                    break;
+                }
+            case KeyboardDef.keyboardTypes.WindowsPortable3 :
+                {
+                    keyboard = new WindPortable3();
+                    keyboard.MakeIt104Key();
+                    keyboard.MakeEnterFlat();
+                    primaryKeyList = keyboard.GetPrimaryKeyList;
+                    secondaryKeyList = keyboard.GetSecondaryKeyList; 
+                    break;
+                }
+            case KeyboardDef.keyboardTypes.WindowsErgonomic1 :
+                {
+                    keyboard = new WinErgonomic1();
+                    keyboard.MakeIt104Key();
+                    keyboard.MakeEnterFlat();
+                    primaryKeyList = keyboard.GetPrimaryKeyList;
+                    secondaryKeyList = keyboard.GetSecondaryKeyList; 
+                    break;
+                }
+            case KeyboardDef.keyboardTypes.WindowsErgonomic2 :
+                {
+                    keyboard = new WinErgonomic2();
+                    keyboard.MakeIt104Key();
+                    keyboard.MakeEnterFlat();
+                    primaryKeyList = keyboard.GetPrimaryKeyList;
+                    secondaryKeyList = keyboard.GetSecondaryKeyList; 
+                    break;
+                }
+            case KeyboardDef.keyboardTypes.WindowsPocket1 :
+                {
+                    keyboard = new WindPocket1();
+                    keyboard.MakeIt104Key();
+                    keyboard.MakeEnterFlat();
+                    primaryKeyList = keyboard.GetPrimaryKeyList;
+                    secondaryKeyList = keyboard.GetSecondaryKeyList; 
+                    break;
+                }
+            case KeyboardDef.keyboardTypes.MacDesktop :
+                {
+                    keyboard = new MacDesktop();
+                    keyboard.MakeIt104Key();
+                    keyboard.MakeEnterFlat();
+                    primaryKeyList = keyboard.GetPrimaryKeyList;
+                    secondaryKeyList = keyboard.GetSecondaryKeyList; 
+                    break;
+                }
+            case KeyboardDef.keyboardTypes.MacPortable :
+                {
+                    keyboard = new MacPortable();
+                    keyboard.MakeIt104Key();
+                    keyboard.MakeEnterFlat();
+                    primaryKeyList = keyboard.GetPrimaryKeyList;
+                    secondaryKeyList = keyboard.GetSecondaryKeyList; 
+                    break;
+                }
+            default:
+                break;
+        }
+
+        // test //////////////////////////
+        //////////////////
+        ////////
+        primaryKeyList.RemoveRange(0,30);
     }
 }
